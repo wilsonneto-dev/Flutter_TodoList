4ac54078c0be4a9a4b9301e76f0848f143cbe9e0
Wilson Neto Dev <contato@wilsonneto.com.br>
2020-02-12 10:46:09 -0300

    tasklist widget

--- lib/screens/todolist.dart
+++ lib/screens/todolist.dart
@@ -21,7 +21,34 @@ class TodoListState extends State<TodoList> {
 
     return Scaffold(
       body: todoListItems(),
-      floatingActionButton: ,
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: "Add To-Do",
+      ),
+    );
+  }
+
+  ListView todoListItems() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Text(this.todos[position].id.toString()),
+            ),
+            title: Text(this.todos[position].title),
+            subtitle: Text(this.todos[position].date),
+            trailing: Icon(Icons.track_changes),
+            onTap: () {
+              debugPrint("taped in ${this.todos[position].title}");
+            },
+          )
+        );
+      }
     );
   }
 
